6bc72d3121d15346241a62fc266729dc3c8da3e1
Hanna <hannamuzychuk@gmail.com>
2026-04-09 18:59:29 +0200

    feat: upate path in import

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
-import { ArticleModule } from 'src/article/article.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [ArticleModule],
+  imports: [PrismaModule],
   controllers: [CommentController],
   providers: [CommentService],
 })
