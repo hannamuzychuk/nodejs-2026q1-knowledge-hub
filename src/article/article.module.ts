303bd0c2797721d545463759acb9c5f9c836a767
Hanna <hannamuzychuk@gmail.com>
2026-05-03 10:42:38 +0200

    chore: register AiModule and export ArticleService for AI

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from '../prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [ArticleController],
   providers: [ArticleService],
+  exports: [ArticleService],
 })
 export class ArticleModule {}
