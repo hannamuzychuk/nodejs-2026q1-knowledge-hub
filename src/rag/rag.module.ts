762cb6a610ed0d57e522e0c3174e7865c5475ca8
Hanna <hannamuzychuk@gmail.com>
2026-05-09 12:05:30 +0200

    feat: add Qdrant vector responce

--- src/rag/rag.module.ts
+++ src/rag/rag.module.ts
@@ -3,9 +3,15 @@ import { RagController } from './rag.controller';
 import { RagService } from './rag.service';
 import { RagChunkerService } from './chunking/rag-chunker.service';
 import { GeminiEmbeddingService } from './gemini-embedding.service';
+import { QdrantRepository } from './vector-db/qdrant.repository';
 
 @Module({
   controllers: [RagController],
-  providers: [RagService, RagChunkerService, GeminiEmbeddingService],
+  providers: [
+    RagService,
+    RagChunkerService,
+    GeminiEmbeddingService,
+    QdrantRepository,
+  ],
 })
 export class RagModule {}
